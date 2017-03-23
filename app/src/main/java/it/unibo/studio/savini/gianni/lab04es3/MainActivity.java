798a081bda2d8318c5797c2d664448e7be8633e3
Gianni Savini <gianni.savini@studio.unibo.it>
2017-03-23 16:48:35 +0100

    Now when i click the element, i show a Toast

--- app/src/main/java/it/unibo/studio/savini/gianni/lab04es3/MainActivity.java
+++ app/src/main/java/it/unibo/studio/savini/gianni/lab04es3/MainActivity.java
@@ -2,6 +2,8 @@ package it.unibo.studio.savini.gianni.lab04es3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toast.makeText(MainActivity.this, getString(R.string.string), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.action_add:
+                Toast.makeText(MainActivity.this, "Add Cliccato", Toast.LENGTH_SHORT).show();
+                break;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
     }
 }
